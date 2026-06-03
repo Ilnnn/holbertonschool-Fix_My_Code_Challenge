85828a062b62ceae7360346880eeb85a82e68237
Ilan <camoreze@gmail.com>
2026-06-03 10:57:06 +0200

    Rearrange FizzBuzz checks for common multiples
    Reordered checks for FizzBuzz logic to prioritize common multiples.

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -7,13 +7,17 @@ import sys
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
 
     tmp_result = []
     for i in range(1, n + 1):
-        # On vérifie le multiple commun en priorité
+        """ Moved this up to be checked first. """
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
