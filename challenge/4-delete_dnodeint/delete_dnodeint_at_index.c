dbab94ef95d9ba99a217eb9033a717c1acfe8bb0
Ilan <camoreze@gmail.com>
2026-06-03 11:14:30 +0200

    Refactor delete_dnodeint_at_index function

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,37 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp = *head;
-    unsigned int i = 0;
+	dlistint_t *tmp;
+	unsigned int p = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    /* 1. Trouver le nœud à supprimer */
-    while (tmp != NULL && i < index)
-    {
-        tmp = tmp->next;
-        i++;
-    }
+	tmp = *head;
+	while (p < index && tmp != NULL)
+	{
+		tmp = tmp->next;
+		p++;
+	}
 
-    /* Si l'index est hors limites */
-    if (tmp == NULL)
-        return (-1);
+	if (tmp == NULL)
+		return (-1);
 
-    /* 2. Suppression de la tête */
-    if (tmp == *head)
-    {
-        *head = tmp->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-    }
-    /* 3. Suppression au milieu ou à la fin */
-    else
-    {
-        tmp->prev->next = tmp->next;
-        if (tmp->next != NULL)
-            tmp->next->prev = tmp->prev;
-    }
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
 
-    free(tmp);
-    return (1);
+	free(tmp);
+	return (1);
 }
