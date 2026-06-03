10349aeb0a58e90a6ad5b8dc912418c2de5787e9
Ilan <camoreze@gmail.com>
2026-06-03 11:15:10 +0200

    Refactor FizzBuzz logic for improved readability
    Reorganized FizzBuzz logic for clarity by moving the condition checks up in the loop.

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -17,14 +17,15 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-    if (i % 3) == 0 and (i % 5) == 0:
-        tmp_result.append("FizzBuzz")
-    elif (i % 3) == 0:
-        tmp_result.append("Fizz")
-    elif (i % 5) == 0:
-        tmp_result.append("Buzz")
-    else:
-        tmp_result.append(str(i))
+        """ Moved this up to be checked first. """
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
 
@@ -35,5 +36,7 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
+    number = int(sys.argv[1])
+    fizzbuzz(number)
     number = int(sys.argv[1])
     fizzbuzz(number)
